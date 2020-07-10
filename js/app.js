188046dda88e6d2cc79fb055853c5740fb79c3a5
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-07-10 16:49:11 -0700

    implemented basic functions

--- js/app.js
+++ js/app.js
@@ -13,10 +13,18 @@
  * 
 */
 
+
+
+
 /**
  * Define Global Variables
  * 
 */
+
+const sectionElements = document.querySelectorAll('section');
+const fragment = document.createDocumentFragment();
+const navbar = document.getElementById("navbar__list");
+
 
 
 /**
@@ -24,6 +32,8 @@
  * Start Helper Functions
  * 
 */
+
+window.addEventListener('DOMContentLoaded',function(){window.scrollTo(0,0)})
 
 
 
@@ -33,10 +43,66 @@
  * 
 */
 
-// build the nav
+/* build nav */
+
+/* get all the section */
+/* insert li elements using document fragments*/
+/* innerhtml = <a href = ??> data-nav(getAttribute)<a> */
+function addNavList() {
+    for (const elem of sectionElements) {
+        const newLi = document.createElement("li");
+        const newLiContent = `<a href="#" data-list=${elem.id}>${elem.getAttribute("data-nav")}</a>`;
+        newLi.innerHTML=newLiContent;
+        // console.log(newLi);
+        fragment.appendChild(newLi);
+    }
+    navbar.appendChild(fragment);
+}
 
 
 // Add class 'active' to section when near top of viewport
+/* get section offset
+set element class name */
+
+function addActiveClassToElem(){
+    const navLists = document.querySelectorAll("#navbar__list li");
+    for (let i = 0; i<sectionElements.length;i++){
+        if (window.pageYOffset >= (sectionElements[i].offsetTop-250) && window.pageYOffset <= (sectionElements[i].offsetTop + sectionElements[i].offsetHeight-250)){
+            sectionElements[i].classList.add("section-active");
+            navLists[i].classList.add("navbar-active");
+
+        }else{
+            sectionElements[i].classList.remove("section-active");
+            navLists[i].classList.remove("navbar-active");
+        }
+    }
+}
+
+function handleScrolled(){
+    window.addEventListener('scroll',addActiveClassToElem);
+}
+
+
+function scrollToSection(event){
+    if(event.target.nodeName === "A"){
+        event.preventDefault();
+        window.scrollTo(0,document.getElementById(event.target.getAttribute("data-list")).offsetTop);
+    }
+
+}
+
+function handleClick(){
+    navbar.addEventListener('click',scrollToSection)
+}
+
+
+// build the nav
+
+addNavList();
+
+// Add class 'active' to section when near top of viewport
+handleScrolled();
+handleClick();
 
 
 // Scroll to anchor ID using scrollTO event
@@ -45,7 +111,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
@@ -53,5 +119,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
